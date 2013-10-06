91d05dffc341b6d671488b512e2eb6cbeb98246e
MIchaelKa <michael.kalinin.nn@gmail.com>
2013-10-06 20:18:02 +0300

    Initial commit

---
+++ tm.rb
@@ -0,0 +1,30 @@
+# Touring Turing
+# or...
+# simulation of
+# Turing Machine
+# 
+# based on
+# Charles Wetherell - "Etudes for programmers"
+# 
+# code by
+# Michael Kalinin
+# 2013
+
+class TuringMachine
+
+  def initialize
+    @programm
+
+    @tape_head
+
+    @control_unit
+
+    @tape
+    @current_state
+    @position
+  end
+
+  def run
+  end
+
+end
