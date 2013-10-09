cdb2412d045f1ed11d002cfd62f1bc9d469597e2
MIchaelKa <michael.kalinin.nn@gmail.com>
2013-10-09 23:23:35 +0300

    first simple implementation

--- tm.rb
+++ tm.rb
@@ -12,19 +12,73 @@
 
 class TuringMachine
 
+  RIGHT = :right
+  LEFT  = :left
+  NONE  = :none
+
+  INITIAL_STATE = 1
+  END_STATE     = 0
+
+  INITIAL_POS   = 0
+
   def initialize
-    @programm
+
+    @state_number = 4;
+    @character_number = 3;
+
+    @programm = Array.new(@state_number) {
+                Array.new(@character_number) {
+                Hash.new()
+                }}
+
+    #program
+    @programm[INITIAL_STATE][1] = { state: 1, character: 1, direction: RIGHT}
+    @programm[INITIAL_STATE][0] = { state: 2, character: 1, direction: RIGHT}
+    @programm[2][1] = { state: 2, character: 1, direction: RIGHT}
+    @programm[2][2] = { state: 3, character: 2, direction: LEFT}
+    @programm[3][1] = { state: 3, character: 2, direction: NONE}
+    @programm[3][2] = { state: END_STATE, character: 2, direction: NONE}
+
+
+    @current_state = INITIAL_STATE
+    @position      = INITIAL_POS
+
+    @tape = [1,1,0,1,1,1,2]
 
     @tape_head
-
     @control_unit
 
     @tape
-    @current_state
-    @position
   end
 
   def run
+
+    while @current_state != END_STATE do
+      print_tape
+      character  = @tape[@position]
+      state      = @current_state
+      @current_state = @programm[state][character][:state]
+      @tape[@position] = @programm[state][character][:character]
+      case @programm[state][character][:direction]
+      when RIGHT
+        @position += 1
+      when LEFT
+        @position -= 1
+      end
+    end
   end
 
+  def print_tape
+
+    @tape.each do |i|
+      print i
+      print " "
+    end
+    print "\n"
+    @position.times { print "  " }
+    print "^"
+    print "\n"
+  end
 end
+
+TuringMachine.new.run()
